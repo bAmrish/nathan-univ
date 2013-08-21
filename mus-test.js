8a01c16d9aa08513df6d97983d9e86ac179ebb9b
Amrish Bharatiya <abharatiya@affinnova.com>
2013-08-21 09:28:28 -0400

    Initial commit

---
+++ mus-test.js
@@ -0,0 +1,115 @@
+
+
+
+// TESTS FOR prelude
+
+var melody1 = { tag: 'note', pitch: 'c4', dur: 250 };
+var melody2 = 
+    { tag: 'seq',
+      left: { tag: 'note', pitch: 'd4', dur: 500 },
+      right: { tag: 'note', pitch: 'c4', dur: 250 } };
+var melody3 = 
+    { tag: 'seq',
+      left: { tag: 'note', pitch: 'd4', dur: 500 },
+      right:
+       { tag: 'seq',
+         left: { tag: 'note', pitch: 'd4', dur: 500 },
+         right: { tag: 'note', pitch: 'c4', dur: 250 } } };
+
+jshint(answer);
+eval(answer);
+
+assert_eq(prelude(melody1), melody2,
+       'Single note input prelude test');
+assert_eq(prelude(melody2), melody3,
+       'Double note input prelude test');
+
+// TESTS for reverse
+
+var melody1 = { tag: 'note', pitch: 'a4', dur: 125 };
+var melody2 = 
+    { tag: 'seq',
+      left: 
+       { tag: 'seq',
+         left: { tag: 'note', pitch: 'a4', dur: 250 },
+         right: { tag: 'note', pitch: 'b4', dur: 250 } },
+      right:
+       { tag: 'seq',
+         left: { tag: 'note', pitch: 'c4', dur: 500 },
+         right: { tag: 'note', pitch: 'd4', dur: 500 } } };
+var melody3 = 
+    { tag: 'seq',
+      left:
+       { tag: 'seq',
+         left: { tag: 'note', pitch: 'd4', dur: 500 },
+         right: { tag: 'note', pitch: 'c4', dur: 500 } },
+      right:
+       { tag: 'seq',
+         left: { tag: 'note', pitch: 'b4', dur: 250 },
+         right: { tag: 'note', pitch: 'a4', dur: 250 } } };
+
+jshint(answer);
+eval(answer);
+
+assert_eq(reverse(melody1), melody1,
+       'One note test');
+assert_eq(reverse(melody2), melody3,
+       'Four note test');
+assert_eq(reverse(melody3), melody2,
+       'Four note test backwards');
+
+//TEST for endTime
+
+var melody1_mus = { tag: 'note', pitch: 'a4', dur: 125 };
+var melody2_mus = 
+    { tag: 'seq',
+      left: 
+       { tag: 'seq',
+         left: { tag: 'note', pitch: 'a4', dur: 250 },
+         right: { tag: 'note', pitch: 'b4', dur: 250 } },
+      right:
+       { tag: 'seq',
+         left: { tag: 'note', pitch: 'c4', dur: 500 },
+         right: { tag: 'note', pitch: 'd4', dur: 500 } } };
+
+jshint(answer);
+eval(answer);
+
+assert_eq(endTime(500, melody1_mus), 625,
+       'One note test'); 
+assert_eq(endTime(0, melody2_mus), 1500,
+       'Four note test');
+
+
+//TEST for compile
+
+var melody1_mus = { tag: 'note', pitch: 'a4', dur: 125 };
+var melody1_note = [ 
+    { tag: 'note', pitch: 'a4', start: 0, dur: 125 } ];
+var melody2_mus = 
+    { tag: 'seq',
+      left: 
+       { tag: 'seq',
+         left: { tag: 'note', pitch: 'a4', dur: 250 },
+         right: { tag: 'note', pitch: 'b4', dur: 250 } },
+      right:
+       { tag: 'seq',
+         left: { tag: 'note', pitch: 'c4', dur: 500 },
+         right: { tag: 'note', pitch: 'd4', dur: 500 } } };
+var melody2_note = [
+    { tag: 'note', pitch: 'a4', start: 0, dur: 250 },
+    { tag: 'note', pitch: 'b4', start: 250, dur: 250 },
+    { tag: 'note', pitch: 'c4', start: 500, dur: 500 },
+    { tag: 'note', pitch: 'd4', start: 1000, dur: 500 } ];
+var endTime = function (time, expr) {
+    if (expr.tag === 'note') return time + expr.dur;
+    return endTime(endTime(time, expr.left), expr.right);
+};
+
+jshint(answer);
+eval(answer);
+
+assert_eq(compile(melody1_mus), melody1_note,
+       'One note test');
+assert_eq(compile(melody2_mus), melody2_note,
+       'Four note test');
---
+++ mus.js
@@ -0,0 +1,130 @@
+/*
+* Write a function prelude that takes a music expression expr
+* as input and returns an expression that means to play a d4 note
+* for 500 milliseconds and then play expr.
+*/
+var prelude = function(expr) {
+    return {
+        tag: 'seq',
+        left: {
+            tag: 'note', pitch: 'd4', dur: 500
+        },
+        
+        right: expr
+    };
+};
+
+
+/**
+* This time write a function reverse that takes
+* a music expression as input and returns a new music 
+* expression that plays the notes in the reverse order. 
+* Your function shouldn't modify the input, it should 
+* just return a new reversed expression.
+*/
+var reverse = function(expr){
+	var newExpr = {};
+
+	if(expr.tag === 'note'){
+		newExpr = expr;
+	}else if (expr.tag === 'seq' ) {
+		newExpr.tag = expr.tag;
+		newExpr.left = reverse(expr.right);
+		newExpr.right = reverse(expr.left);
+	} else {
+		throw new Error("Invalid expression syntax.");
+	}
+
+	return newExpr;
+};
+
+/**
+* Write a function endTime that takes a start time 
+* time in milliseconds and a MUS expression expr.
+* Assuming expr starts playing at time time, 
+* the function should return the time when expr finishes.
+*/
+var endTime = function (time, expr) {
+	if(!expr){
+		throw new Error("Invalid expression syntax.");
+	}
+
+	if(expr.tag === 'note'){
+		return time + expr.dur;	
+	} else if (expr.tag === 'seq' ) {
+		return endTime( endTime(time, expr.left), expr.right);
+	} else {
+		throw new Error("Invalid expression syntax.");
+	}	
+};
+
+/*
+* Write a function compile that compiles MUS songs into NOTE songs.
+* 
+*  Example:
+* 
+*  MUS Song:
+*  =========
+*  { tag: 'seq',
+*   left: 
+*    { tag: 'seq',
+*      left: { tag: 'note', pitch: 'a4', dur: 250 },
+*      right: { tag: 'note', pitch: 'b4', dur: 250 } },
+*   right:
+*    { tag: 'seq',
+*      left: { tag: 'note', pitch: 'c4', dur: 500 },
+*      right: { tag: 'note', pitch: 'd4', dur: 500 } } }
+* 
+* Compiled NOTE version
+* ====================
+* [ { tag: 'note', pitch: 'a4', start: 0, dur: 250 },
+*   { tag: 'note', pitch: 'b4', start: 250, dur: 250 },
+*   { tag: 'note', pitch: 'c4', start: 500, dur: 500 },
+*   { tag: 'note', pitch: 'd4', start: 1000, dur: 500 } ]
+* 
+*/
+
+var compile = function(mus){	
+	var notes = [];
+	var queue = [];
+	var expr = null;
+	var totalTime = 0;
+
+	var addToTop = function(array, element){
+		array.splice(0, 0, element);
+		return array;
+	};
+
+	var popFirst = function(array){
+		return array.splice(0, 1)[0];
+	};
+
+
+	queue.push(mus);
+
+	while(queue.length){
+		expr = popFirst(queue);
+		
+		if(notes.length){
+			lastExpr = notes[notes.length - 1];
+		}
+
+		if(expr.tag === 'note'){
+			notes.push({
+				tag: expr.tag,
+				pitch: expr.pitch,
+				start: totalTime,
+				dur: expr.dur
+			});
+
+			totalTime = endTime(totalTime, expr);
+		}else if(expr.tag === 'seq'){
+			addToTop(queue, expr.right);
+			addToTop(queue, expr.left);
+		}else{
+			throw new Error("Invalid expression syntax.");	
+		}
+	}
+
+	return notes;
+};
