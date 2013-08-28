b39f3500f3395b926bca5e6aec237c2173799e57
Amrish Bharatiya <abharatiya@affinnova.com>
2013-08-28 17:02:18 -0400

    Slightly refactored mus-grammer-tests

--- tests/mus-grammer-test.js
+++ tests/mus-grammer-test.js
@@ -1,9 +1,29 @@
 var expect = require('expect.js');
-var tester = require('./tester.js')
+var tester = require('./tester.js');
 
-tester.read('grammers/mus-grammer.pegjs', function(){			
-	expect(tester.eval("a1[100]")).to.eql({ tag: 'note', pitch: 'a1', dur: 100 });
-	expect(tester.eval("g8[20]")).to.eql({ tag: 'note', pitch: 'g8', dur: 20 });
-	expect(tester.eval("h1[120]")).to.eql(undefined);
-	expect(tester.eval("e10[120]")).to.eql(undefined);
+tester.read('grammers/mus-grammer.pegjs', function(){		
+
+	var tests = [
+		{
+			input: "a1[100]",
+			expected: { tag: 'note', pitch: 'a1', dur: 100 }
+		},
+		{
+			input: "g8[20]",
+			expected: { tag: 'note', pitch: 'g8', dur: 20 }
+		},
+		{
+			input: "h1[120]",
+			expected: undefined
+		},
+		{
+			input: "e10[120]",
+			expected: undefined
+		}
+	];
+
+	tests.forEach(function(test){
+		expect(tester.eval(test.input)).to.eql(test.expected);			
+	});
+	
 });
